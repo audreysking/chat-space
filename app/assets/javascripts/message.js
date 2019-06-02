08c782f4b5b4caca867b5890f98e6f83979bba0e
Jin Ruixin <audreysking@gmail.com>
2019-06-02 16:47:58 +0900

    update send message

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,51 @@
+$(document).on('turbolinks:load', function(){
+
+  function buildHTML(message){
+    var content = message.content ? `${message.content} ` : ''
+    var image = message.image.url ? `<image src='${message.image.url}', class: 'lower-message__image'>` : ''
+
+    var html = `<div class = "message">
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class = "upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">
+                      ${content}
+                    </p>
+                    ${image}
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+      $('.form__message').val('');
+      $('.hidden').val('');
+      $('.form__submit').removeAttr("disabled");
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+
+});
