1e3a256aa915c743abe9e3c458eae20e3ca2a59f
Jin Ruixin <audreysking@gmail.com>
2019-06-04 14:39:27 +0900

    update auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(document).on('turbolinks:load', function(){
     var content = message.content ? `${message.content} ` : ''
     var image = message.image.url ? `<image src='${message.image.url}', class: 'lower-message__image'>` : ''
 
-    var html = `<div class = "message">
+    var html = `<div class = "message" data-id=${message.id}>
                   <div class = "upper-message">
                     <div class = "upper-message__user-name">
                       ${message.user_name}
@@ -47,4 +47,33 @@ $(document).on('turbolinks:load', function(){
     })
   })
 
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var group_id = location.pathname.split('/')[2]
+    var last_message_id = $(".message").last().data('id');
+    var url = location.pathname.replace("messages","api/messages.json")
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json',
+      data: {group_id,last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      if (messages.length !== 0) {
+        messages.forEach(function(message){
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML)
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (location.pathname.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 5000);
+  }
 });
